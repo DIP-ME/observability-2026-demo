ad9906b1df54da940d668f9ef09265800e6bfbbe
skillupwithsachin <168949746+skillupwithsachin@users.noreply.github.com>
2026-08-29 09:47:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    "app_requests_total",
+    "Total number of application requests"
+)
+
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello from Observability Demo!"
+
+
+@app.route("/health")
+def health():
+    REQUEST_COUNT.inc()
+    return "OK"
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype="text/plain"
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
